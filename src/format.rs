1643f0d94538ccb1e22077ad43808e0f286f41f2
Stefan Walter <stefan_walter@posteo.de>
2025-05-06 06:05:34 +0200

    add license information with `reuse`

--- src/format.rs
+++ src/format.rs
@@ -1,1 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Paper and original Racket Code: Małgorzata Biernacka, Witold Charatonik, and Tomasz Drab. 2022. A simple and efficient implementation of strong call by need by an abstract machine. Proc. ACM Program. Lang. 6, ICFP, Article 94 (August 2022), 28 pages. https://doi.org/10.1145/3549822
+// SPDX-License-Identifier: CC-BY-4.0
+//
+// SPDX-FileCopyrightText: 2025 This specific implementation: Stefan Walter
+// SPDX-License-Identifier: MIT
+
 pub mod named;
